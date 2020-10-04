e7e6830172c38cd46ba69e74f605f3bc58629144
Yamin Thwe <71778915+Yamin-Thwe-5078@users.noreply.github.com>
2020-10-05 01:13:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -26,69 +26,31 @@
         
         <meta charset="utf-8">
         
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  	<link rel="stylesheet" href="/resources/demos/style.css">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         
         <title>Weatherapp</title>
-        
-        <style type="text/css">
-            
-            body{
-                
-                padding:0;
-                margin:0;
-                font-family:Arial, Helvetica, sans-serif;
-                background-image:url("Unsplash1.jpg");
-                background-size:cover;
-                
-            }
-            
-            #heading{
-                
-                color:#f6d113;
-                text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;
-                text-align:center;
-                
-            }
-            
-            #middle{
-                
-                position: absolute;
-                top: 50%;
-                left: 50%;
-                transform: translate(-50%, -50%);
-                
-            }
-            
-            #label{
-                
-                font-size:20px;
-                text-align:center;
-                font-weight:bold;
-                
-            }
-            
-            input{
-                
-                margin:30px 0px;
-                
-            }
-            
-            #myBtn{
-                
-                display:none;
-                
-            }
-            
-            .container{
-                
-                width:700px;
-                
-            }
-            
-        </style>
+
+ 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+	<link rel="stylesheet" type="text/css" href="weather.css">
+	
+        <script type="text/javascript" src="jquery.min.js"></script>
+	<link href="jquery-ui/jquery-ui.css" rel="stylesheet">
+       <script>
+		
+		$( function() {
+    			var src = ["Chennai","Bangalore","Delhi","Mumbai","Hyderabad","New Delhi","Kolkata","Agra","London","New York","Paris","Moscow","Tokyo","Dubai","Singapore","Los Angeles","Seoul","Beijing","Las Vegas","Bangkok","Ahmedabad","Pune","Amravati","Visakhapatnam","Surat","Jaipur","Nagpur","Barcelona","Madrid","Rome","Chicago","Toronto","San Francisco","Abu Dhabi","St.Petersburg","Amsterdam","Berlin","Prague","Washington","Istanbul","Doha","Sydney","Miami","Munich","Milan","San Diego","Vienna","Dublin","Vancouver","Boston","Zurich","Melbourne","Budapest","Houstan","Seattle","Montreal","Frankfurt","Sao Paulo","Tel Aviv","Copenhagen","Calgary","Orlando","Atlanta","Dallas","Hamburg","Osaka","Lisbon","Austin","Phoenix","Naples","Oslo","Denver","Stockholm","Philadelphia","Riyadh","Buenos Aires","San Jose","Brussels","Portland","Ottawa","Helsinki","Valencia","Brisbane","Warsaw","Minneapolis","Shangai","Lyon","Adelaide","Edmonton","Marseille","Muscat","Athens","Stuttgart","Rio De Janeiro","Baltimore","Auckland","Cologne","New Orleans","Kuwait","Kiev","Hanover","Perth","Minsk","Bucharest","Nashville","Dusseldorf","Manchester","Sacramento","Glasgow","Mexico","Salt Lake","Raleigh","Kracow","Coimbatore","Madurai","Udhagamandalam","Ooty","Tiruchirappalli","Tirunelveli","Salem","Pondicherry","Ambur","Cuddalore","Dindigul","Erode","Gudiyatham","Hosur","Kancheepuram","Karaikkudi","Karur","Kumarapalayam","Kumbakonam","Nagapattinam","Nagercoil","Neyveli","Pollachi","Pudukkottai","Rajapalayam","Ranipet","Sivakasi","Thanjavur","Thoothukkudi","Tiruvannamalai","Tiruppur","Vaniyambadi","Vellore","Agartala","Aizawl","Allahabad","Amritsar","Atal Nagar","Aurangabad","Bhopal","Bhubaneswar","Chandigarh","Dehradun","Dispur","Gandhinagar","Gangtok","Gurgaon","Imphal","Indore","Itanagar","Jalandhar","Jamshedpur","Jodhpur","Kanpur","Kochi","Kohima","Lucknow","Mangalore","Mysore","Nashik","Navi Mumbai","Naya Raipur","Noida","Panaji","Patna","Port Blair","Ranchi","Shillong","Shimla","Srinagar","Thane","Thiruvananthapuram","Udaipur","Varanasi","Vijayawada","Aligarh","Bareilly","Dhanbad","Faridabad","Ghaziabad","Gwalior","Guwahati","Howrah","Hubli–Dharwad","Jabalpur","Kalyan-Dombivli","Ludhiana","Meerut","Moradabad","Nashik","Pimpri-Chinchwad","Rajkot","Solaipur","Vadodara","Vasai-Virar","Warangal","A Coruña","Aachen","Aalborg","Aarhus","Aba","Abadan","Abaetetuba","Abakan","Abbotabad","Abbotsford","Abengourou","Abeokuta","Aberdeen","Abha","Abidjan","Abiko","Abilene","Abohar","Abu Al-Khaseeb","Abu Ghraib","Abuja","Açailândia","Acapulco","Acarigua","Accra","Achalpur","Acheng","Achinsk","Adama","Adana","Adapazarı","Addis Ababa","Aden","Adhamiyah","Adilabad","Adityapur","Adıyaman","Ado-Ekiti","Adoni","Cairo","Cali",
+      
+    			];
+    			$( "#fInput" ).autocomplete({
+      				source: function(request, response) {
+        				var results = $.ui.autocomplete.filter(src, request.term);
+        				response(results.slice(0, 5));
+				}
+    			});
+  		} );
+
+	</script>
         
     </head>
     
@@ -102,7 +64,7 @@
             
             <form>
                 
-                <div class="form-group ui-widget">
+                <div class="ui-widget">
     
                     <input type="text" name="input" class="form-control" id="fInput" placeholder="E.g: London, Chennai, Tokyo">
                     <input type="submit" id="myBtn">
@@ -131,20 +93,20 @@
             
         </div>
 
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-  
         
+
+  
+        <script src="jquery-ui/jquery-ui.js"></script>
+
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
         
         <script type="text/javascript">
             
-            var input = $("#myInput");
+            //var input = $("#fInput");
 
-            input.addEventListener("keyup", function(e) {
+            $("#fInput").keyup(function(e) {
 
                 if (e.keyCode === 13 || e.which === 13) {
 
@@ -156,16 +118,6 @@
                 
             });
 
-		$(function() {
-    			var availableTags = [
-				"Chennai",
-				"Cairo",
-				"Cali"
-			];
-    			$("#fInput").autocomplete({
-      				source: availableTags
-    			});
-  		} );
             
         </script>
         
